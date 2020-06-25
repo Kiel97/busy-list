07915ebbe40b1877abaf3893da375d230fd31b69
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-06-25 13:34:58 +0200

    Replanned database schema for future use

--- components/pages/welcome-screen.js
+++ components/pages/welcome-screen.js
@@ -19,17 +19,22 @@ function WelcomeScreen({navigation, route}){
             txn.executeSql('DROP TABLE IF EXISTS "lists"', []);
             txn.executeSql('DROP TABLE IF EXISTS "tasks"', []);
             txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS "lists"("id" INTEGER NOT NULL UNIQUE,"listName" TEXT NOT NULL, PRIMARY KEY("id" AUTOINCREMENT));',
+              'CREATE TABLE IF NOT EXISTS "lists"("id" INTEGER NOT NULL UNIQUE,"listName" TEXT NOT NULL, "favourite" INTEGER NOT NULL DEFAULT 0 CHECK ("favourite"=0 or "favourite"=1), created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY("id" AUTOINCREMENT));',
               []
             );
             txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS "tasks"("id" INTEGER NOT NULL UNIQUE, "listId" INTEGER NOT NULL, "taskName" TEXT NOT NULL, "done" INTEGER NOT NULL DEFAULT 0 CHECK("done"=0 OR "done"=1), PRIMARY KEY("id" AUTOINCREMENT), FOREIGN KEY("listId") REFERENCES "lists"("id") ON DELETE CASCADE);',
+              'CREATE TABLE IF NOT EXISTS "tasks"("id" INTEGER NOT NULL UNIQUE, "listId" INTEGER NOT NULL, "taskName" TEXT NOT NULL, "done" INTEGER NOT NULL DEFAULT 0 CHECK("done"=0 OR "done"=1), "note" TEXT, "tag" TEXT, "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY("id" AUTOINCREMENT), FOREIGN KEY("listId") REFERENCES "lists"("id") ON DELETE CASCADE);',
               []
             );
             if (prepopulateDB) {
               tx.executeSql(
-                'INSERT INTO "lists" ("id", "listname") VALUES (?,?),(?,?),(?,?),(?,?)',
-                [1, 'Spotkanie z Tomaszem', 2, 'Lista zakupów', 3, 'Projekt z RN', 4, 'Prace w ogrodzie'],
+                'INSERT INTO "lists" ("id", "listname", "favourite") VALUES (?,?,?),(?,?,?),(?,?,?),(?,?,?)',
+                [
+                  1, 'Spotkanie z Andrzejem', 0,
+                  2, 'Lista zakupów', 0,
+                  3, 'Projekt z RN', 1,
+                  4, 'Prace w ogrodzie', 0
+                ],
                 (tx, results) => {
                   if (results.rowsAffected > 0) {
                     console.log('Prepopulate "lists": Insert success');
@@ -39,18 +44,18 @@ function WelcomeScreen({navigation, route}){
                 }
               );
               tx.executeSql(
-                'INSERT INTO "tasks" ("id", "listId", "taskName", "done") VALUES (?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?),(?,?,?,?)',
+                'INSERT INTO "tasks" ("id", "listId", "taskName", "done", "note", "tag") VALUES (?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?),(?,?,?,?,?,?)',
                 [
-                  1, 1, 'Przygotowanie sprawozdania końcowego', 1,
-                  2, 1, 'Organizacja spotkania w firmie', 0,
-                  3, 2, 'Świeczki', 1,
-                  4, 2, 'Prezent na urodziny', 0,
-                  5, 2, 'Tort urodzinowy', 0,
-                  6, 3, 'Przejścia między ekranami', 1,
-                  7, 3, 'Integracja z bazą SQLite', 1,
-                  8, 3, 'Pobieranie zadań z Bored API', 1,
-                  9, 3, 'Sprawozdanie', 0,
-                  10, 3, 'Wysłanie projektu', 0,
+                  1, 1, 'Przygotowanie sprawozdania końcowego', 0, 'Muszę koniecznie przygotować sprawozdanie, które przedstawia wyniki naszego departamentu finansów firmy', 'busywork',
+                  2, 1, 'Organizacja miejsca spotkania', 1, 'Muszę znaleźć biuro odpowiednio wielkie na nasz zespół', 'busywork',
+                  3, 2, 'Świeczki', 1, '', '',
+                  4, 2, 'Prezent na urodziny', 0, 'Najlepiej drogi prezent', '',
+                  5, 2, 'Tort urodzinowy', 0, '', 'recreational',
+                  6, 3, 'Przejścia między ekranami', 1, 'Wszystko, co składa się na graficzną reprezentację aplikacji', 'busywork',
+                  7, 3, 'Integracja z bazą danych', 1, 'Połączenie aplikacji z bazą danych SQLite', 'busywork',
+                  8, 3, 'API Sieciowe', 1, 'Bored API świetnie będzie pasował do mojego projektu', 'busywork',
+                  9, 3, 'Inne funkcjonalności', 0, 'Muszę dodać różne rzeczy jak dodawanie/edycja/przegląd opcji listy/zadania, automatyczne odświeżanie list też by się przydało', 'busywork',
+                  10, 3, 'Sprawozdanie', 0, 'Sprawozdanie zawierające udokumentowanie zdjęciami i kodem zaimplementowane funkcjonalności','diy'
                 ],
                 (tx, results) => {
                   if (results.rowsAffected > 0) {
